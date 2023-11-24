7e933411e1235c866114c6969cc4d6e9808aefcd
Sumedh Patkar <sumedh.patkar19@gmail.com>
2023-11-24 11:35:35 -0500

    Added commands for the container to run forever

--- app/startup_script.sh
+++ app/startup_script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "\nStarting the virtual environment"
-source venv/bin/activate
-
-echo "\nInstalling required dependencies"
-pip3 install -r requirements.txt
+# echo "\nInstalling required dependencies"
+# pip3 install -r requirements.txt
 
 echo "\nStarting the flask application in the background"
 python3 app.py &
+
+sleep 5
+
+echo -e "\nIn order for the container not to die after running app"
+tail -f /dev/null
