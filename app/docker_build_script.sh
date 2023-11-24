3582cffaebbb74a6a0d746d3131f8eab10befaf2
Sumedh Patkar <sumedh.patkar19@gmail.com>
2023-11-24 11:36:38 -0500

    Docker build script added. Contains commads. Not used

--- app/docker_build_script.sh
+++ app/docker_build_script.sh
@@ -1,1 +1,12 @@
-docker build -t project-image .
+docker build -t cloud-app .
+docker tag cloud-app sumedhpatkar/cloud-app:latest
+docker push sumedhpatkar/cloud-app:latest
+
+minikube start
+
+kubectl delete deployment cloud-app-deployment
+kubectl delete service cloud-app-deployment
+
+
+kubectl apply -f kube-deployment.yaml
+kubectl get pods
